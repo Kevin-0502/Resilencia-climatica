0ffb7eb91815760695125666615dcc02c6c03fec
Kevin Miguel Jiménez Hernández <hjmkalrevesconfecha@gmail.com>
2023-08-10 14:49:22 -0600

    Change env port

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const { MongoClient } = require("mongodb");
 
 
 //settings
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.set('json spaces', 2);
 
 //mongodb connect
