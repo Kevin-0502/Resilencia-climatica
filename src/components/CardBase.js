5732d86b4bf0c0ad6e5a52653c05b0733167b94a
AlbertoAlas03 <oa3973392@gmail.com>
2023-11-15 23:02:49 -0600

    filter added

--- src/components/CardBase.js
+++ src/components/CardBase.js
@@ -6,170 +6,174 @@ import co2 from "../assests/img/co2.png";
 import planet from "../assests/img/planet-earth.png";
 import ligh_bulb from "../assests/img/light-bulb.png";
 import {
-    StatusOnlineIcon
-  } from "@heroicons/react/outline";
+  StatusOnlineIcon
+} from "@heroicons/react/outline";
 
-function convertCelsiusToFahrenheit (celsius) {
+function convertCelsiusToFahrenheit(celsius) {
   return ((celsius * 1.8) + 32).toFixed(2);
 }
 
-const CardBase = ({last_data}) =>{
+function formatCelsius(celsius) {
+  return (celsius * 1).toFixed(2);
+}
 
-    /*const [last_data,setLast_data]=useState([])
-    
-    function fetch_last_data(){
-      fetch(url_data).then(response=>response.json()).then(resjson=>{
-        setLast_data(resjson[resjson.length-1])
-        console.log(resjson[resjson.length-1])
-      })
-    }
+const CardBase = ({ last_data }) => {
 
-  useEffect(()=>{
-    fetch_last_data()
-  },[]);*/
+  /*const [last_data,setLast_data]=useState([])
+  
+  function fetch_last_data(){
+    fetch(url_data).then(response=>response.json()).then(resjson=>{
+      setLast_data(resjson[resjson.length-1])
+      console.log(resjson[resjson.length-1])
+    })
+  }
 
-    return(
-        <Grid 
-        numItems={1} 
-        numItemsSm={2} 
-        numItemsLg={3} 
-        className="gap-2"
-        >
-            <Col numColSpan={1} numColSpanLg={2}>
-              <Card decoration="top" decorationColor="red">
-                <Flex justifyContent="center">
-                  <Title>Temperatura actual</Title>
-                </Flex>
-                <Flex className="mt-6">
-                  <div>
-                    <img 
-                    alt="Temperatura"
-                    src={thermometer}
-                    style={{
-                        height: 60,
-                        widows: 60
-                      }}
-                    />
-                  </div>
-                  <Metric>{last_data.temperatura}°C / {convertCelsiusToFahrenheit(last_data.temperatura)}°F</Metric>
-                  <Badge icon={StatusOnlineIcon}>LIVE</Badge>
-                </Flex>
-                <Flex className="mt-6">
-                  <Text>Medidor de temperatura (°C)</Text>
-                  <Text>{last_data.temperatura}°C</Text>
-                </Flex>
-                <CategoryBar 
-                  values={[20, 10, 70]}
-                  colors={["sky", "orange","rose"]}
-                  markerValue={last_data.temperatura}
-                  className="mt-3"
-                />
-                <Flex className="mt-6">
-                  <Text>Medidor de temperatura (°F)</Text>
-                  <Text>{convertCelsiusToFahrenheit(last_data.temperatura)}°F</Text>
-                </Flex>
-                <CategoryBar 
-                  values={[68, 18, 14]}
-                  colors={["sky", "orange", "rose"]}
-                  markerValue={convertCelsiusToFahrenheit(last_data.temperatura)}
-                  className="mt-3"
-                />
-                <Legend 
-                  className="mt-3"
-                  categories={["Frío", "Cálido", "Caliente"]}
-                  colors={["blue", "orange", "red"]}
-                />
-              </Card>
-            </Col>
+useEffect(()=>{
+  fetch_last_data()
+},[]);*/
 
-            <Card decoration="top" decorationColor="sky">
-            <Flex justifyContent="center">
-              <Title>Humedad actual</Title>
-            </Flex>
-            <Flex className="mt-6">
-              <div>
-                <img 
-                alt="Humedad"
-                src={humidity_img}
+  return (
+    <Grid
+      numItems={1}
+      numItemsSm={2}
+      numItemsLg={3}
+      className="gap-2"
+    >
+      <Col numColSpan={1} numColSpanLg={2}>
+        <Card decoration="top" decorationColor="red">
+          <Flex justifyContent="center">
+            <Title>Temperatura actual</Title>
+          </Flex>
+          <Flex className="mt-6">
+            <div>
+              <img
+                alt="Temperatura"
+                src={thermometer}
                 style={{
-                    height: 60,
-                    widows: 60
-                  }}
-                />
-              </div>
-              <Metric>{last_data.humedad_relativa}%</Metric>
-              <Badge icon={StatusOnlineIcon}>LIVE</Badge>
-            </Flex>
-            <Flex className="mt-6">
-              <Text className="mt-6">Medidor de humedad</Text>
-              <Text className="mt-6">{last_data.humedad_relativa}%</Text>               
-            </Flex>
-            <ProgressBar value={last_data.humedad_relativa} color="blue" className="mt-3" />
-            <Legend 
-              className="mt-3"
-              categories={["Nivel de húmedad"]}
-              colors={["blue"]}
+                  height: 60,
+                  widows: 60
+                }}
+              />
+            </div>
+            <Metric>{formatCelsius(last_data.temperatura)}°C / {convertCelsiusToFahrenheit(last_data.temperatura)}°F</Metric>
+            <Badge icon={StatusOnlineIcon}>LIVE</Badge>
+          </Flex>
+          <Flex className="mt-6">
+            <Text>Medidor de temperatura (°C)</Text>
+            <Text>{last_data.temperatura}°C</Text>
+          </Flex>
+          <CategoryBar
+            values={[20, 10, 70]}
+            colors={["sky", "orange", "rose"]}
+            markerValue={last_data.temperatura}
+            className="mt-3"
+          />
+          <Flex className="mt-6">
+            <Text>Medidor de temperatura (°F)</Text>
+            <Text>{convertCelsiusToFahrenheit(last_data.temperatura)}°F</Text>
+          </Flex>
+          <CategoryBar
+            values={[68, 18, 14]}
+            colors={["sky", "orange", "rose"]}
+            markerValue={convertCelsiusToFahrenheit(last_data.temperatura)}
+            className="mt-3"
+          />
+          <Legend
+            className="mt-3"
+            categories={["Frío", "Cálido", "Caliente"]}
+            colors={["blue", "orange", "red"]}
+          />
+        </Card>
+      </Col>
+
+      <Card decoration="top" decorationColor="sky">
+        <Flex justifyContent="center">
+          <Title>Humedad actual</Title>
+        </Flex>
+        <Flex className="mt-6">
+          <div>
+            <img
+              alt="Humedad"
+              src={humidity_img}
+              style={{
+                height: 60,
+                widows: 60
+              }}
             />
-            </Card>
-            <Col>
-              <Card decoration="top" decorationColor="stone">
-                <Flex justifyContent="center">
-                  <Title>CO2 registrado</Title>
-                </Flex>
-                <Flex className="mt-6">
-                  <div>
-                    <img 
-                      alt="CO2"
-                      src={co2}
-                      style={{
-                        height: 60,
-                        windows: 60
-                      }}
-                      />
-                  </div>
-                  <Metric>{last_data.CO2} PPM</Metric>
-                  <Badge icon={StatusOnlineIcon}>LIVE</Badge>
-                </Flex>
-              </Card>
-            </Col>
-            <Card decoration="top" decorationColor="emerald">
-              <Flex justifyContent="center">
-                <Title>VOC registrado</Title>
-              </Flex>
-              <Flex className="mt-6">
-                <div>
-                  <img 
-                    alt="planet"
-                    src={planet}
-                    style={{
-                        height: 60,
-                        widows: 60
-                    }}/>
-                </div>
-                <Metric>{last_data.VOC} mg/m3</Metric>
-                <Badge icon={StatusOnlineIcon}>LIVE</Badge>
-              </Flex>
-            </Card>
-            <Card decoration="top" decorationColor="yellow">
-              <Flex justifyContent="center">
-                <Title>Intensidad luminosa</Title>
-              </Flex>
-              <Flex className="mt-6">
-                <div>
-                  <img 
-                  alt="lightbulb"
-                  src={ligh_bulb}
-                  style={{
-                      height: 60,
-                      widows: 60
-                  }}/>
-                </div>
-                <Metric>{last_data.intensidad_luminosa} LUX</Metric>
-                <Badge icon={StatusOnlineIcon}>LIVE</Badge>
-              </Flex>
-            </Card>
-        </Grid>
-    )
+          </div>
+          <Metric>{last_data.humedad_relativa}%</Metric>
+          <Badge icon={StatusOnlineIcon}>LIVE</Badge>
+        </Flex>
+        <Flex className="mt-6">
+          <Text className="mt-6">Medidor de humedad</Text>
+          <Text className="mt-6">{last_data.humedad_relativa}%</Text>
+        </Flex>
+        <ProgressBar value={last_data.humedad_relativa} color="blue" className="mt-3" />
+        <Legend
+          className="mt-3"
+          categories={["Nivel de húmedad"]}
+          colors={["blue"]}
+        />
+      </Card>
+      <Col>
+        <Card decoration="top" decorationColor="stone">
+          <Flex justifyContent="center">
+            <Title>CO2 registrado</Title>
+          </Flex>
+          <Flex className="mt-6">
+            <div>
+              <img
+                alt="CO2"
+                src={co2}
+                style={{
+                  height: 60,
+                  windows: 60
+                }}
+              />
+            </div>
+            <Metric>{last_data.CO2} PPM</Metric>
+            <Badge icon={StatusOnlineIcon}>LIVE</Badge>
+          </Flex>
+        </Card>
+      </Col>
+      <Card decoration="top" decorationColor="emerald">
+        <Flex justifyContent="center">
+          <Title>VOC registrado</Title>
+        </Flex>
+        <Flex className="mt-6">
+          <div>
+            <img
+              alt="planet"
+              src={planet}
+              style={{
+                height: 60,
+                widows: 60
+              }} />
+          </div>
+          <Metric>{last_data.VOC} mg/m3</Metric>
+          <Badge icon={StatusOnlineIcon}>LIVE</Badge>
+        </Flex>
+      </Card>
+      <Card decoration="top" decorationColor="yellow">
+        <Flex justifyContent="center">
+          <Title>Intensidad luminosa</Title>
+        </Flex>
+        <Flex className="mt-6">
+          <div>
+            <img
+              alt="lightbulb"
+              src={ligh_bulb}
+              style={{
+                height: 60,
+                widows: 60
+              }} />
+          </div>
+          <Metric>{last_data.intensidad_luminosa} LUX</Metric>
+          <Badge icon={StatusOnlineIcon}>LIVE</Badge>
+        </Flex>
+      </Card>
+    </Grid>
+  )
 }
 
 
--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import { Grid, Col , DateRangePicker,Card,Button,Flex,DateRangePickerValue } from "@tremor/react";
+import React, { useEffect, useState } from "react";
+import { Grid, Col, DateRangePicker, Card, Button, Flex, DateRangePickerValue } from "@tremor/react";
 import TemperatureChart from "./TemperatureChart";
 import HumidityChart from "./HumidityChart";
 import CO2Chart from "./CO2Chart";
@@ -9,63 +9,75 @@ import url from "./Data";
 
 const ChartsData = () => {
 
-  var url_data =url+'/date'
+  var url_data = url + '/date'
   const [data, setData] = useState([])
   const [dataInitial, setDataInitial] = useState(false);
-  const [value, setValue] = useState(new Date())
+  const [fechaInit, setFechaInit] = useState(new Date());
+  const [fechaFinal, setFechaFinal] = useState(new Date());
 
 
-    
-    async function fetch_data(options) {
-        fetch(url_data,configRequesOptions(options)).then(response => (response).json()).then(resjson => {
-          console.log(resjson.data);
-            if((resjson.data).length>0){
-                setData(resjson.data)
-                setDataInitial(true)
-            }
-        });
-    };
-
-    function configRequesOptions(data){
-      return{
-          method:"POST",
-          headers:{'Content-Type': 'application/json'},
-          body:JSON.stringify(data)
+  async function fetch_data(options) {
+    await fetch(url_data, configRequesOptions(options)).then(response => (response).json()).then(resjson => {
+      console.log(resjson);
+      try {
+        if ((resjson.data).length > 0) {
+          setData(resjson.data)
+          setDataInitial(true)
         }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
+
+  function configRequesOptions(data) {
+    return {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
     }
+  };
 
   useEffect(() => {
-    var date1=new Date('06/01/2023');
-    var date2=new Date('11/28/2023')
-    var dates={
-        initial_date:date1,
-        final_date:date2  
+    var date1 = new Date('11/11/2023');
+    var date2 = new Date();
+    var dates = {
+      initial_date: date1,
+      final_date: date2
     }
     fetch_data(dates);
-}, []);
+  }, []);
 
   return (
     <>
-    <Card><Flex justifyContent="around" >
-      <DateRangePicker enableSelect={false} value={value} onValueChange={value=>setValue(value)}/>
-      <Button color="sky" onClick={()=>{console.log(value)}}>Filtrar</Button>
-    </Flex></Card>
-    <Grid numItems={1} numItemsSm={2} numItemsLg={3} className="gap-2">
+      <Card><Flex justifyContent="around" >
+        <DateRangePicker enableSelect={false} value={fechaInit} onValueChange={fecha1 => setFechaInit(fecha1)} />
+        <DateRangePicker enableSelect={false} value={fechaFinal} onValueChange={fecha2 => setFechaFinal(fecha2)} />
+        <Button color="sky" onClick={() => {
+          alert("boton click");
+          var dates = {
+            initial_date: fechaInit.from,
+            final_date: fechaFinal.from
+          };
+          fetch_data(dates);
+        }}>Filtrar</Button>
+      </Flex></Card>
+      <Grid numItems={1} numItemsSm={2} numItemsLg={3} className="gap-2">
         {/*Card temperatura */}
-    <Col numColSpan={1} numColSpanLg={2}>
-     <TemperatureChart data={data}/> {/*componente graficas temperaturas */}
-    </Col>
+        <Col numColSpan={1} numColSpanLg={2}>
+          <TemperatureChart data={data} /> {/*componente graficas temperaturas */}
+        </Col>
 
-    {/*Card humedad */}
-    <HumidityChart data={data}/> 
-    {/*Card CO2*/}
-    <Col>
-      <CO2Chart data={data}/>
-    </Col>
-    <VOCChart data={data}/>
-    <IntensityChart data={data}/>
-  </Grid>
-  </>
+        {/*Card humedad */}
+        <HumidityChart data={data} />
+        {/*Card CO2*/}
+        <Col>
+          <CO2Chart data={data} />
+        </Col>
+        <VOCChart data={data} />
+        <IntensityChart data={data} />
+      </Grid>
+    </>
   )
 }
 
